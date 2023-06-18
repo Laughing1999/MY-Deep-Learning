bb0dc08cfb4bbd0babe651ee98899a05aa93ba41
LFY <laifuyin69@gmail.com>
2023-06-18 10:46:06 +0800

    Create simple_cnn.py
    add simple_cnn.py file. it is a .py file about training a simple cnn model based on internet data-mnist.

---
+++ simple_cnn.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+# 加载MNIST数据集
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# 数据预处理
+x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# 构建CNN模型
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# 编译模型
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# 训练模型
+model.fit(x_train, y_train, batch_size=128, epochs=5, validation_data=(x_test, y_test))
+
+# 评估模型
+loss, accuracy = model.evaluate(x_test, y_test)
+print("Test Loss:", loss)
+print("Test Accuracy:", accuracy)
